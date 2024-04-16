11476700939a6254504e60e342fb28d8476f4ec0
Harish Patil <harishap000@gmail.com>
2024-04-16 23:57:45 +0530

    first commit

---
+++ 30_Days_of_Code/Day_0_Hello_World/Solution.java
@@ -0,0 +1,18 @@
+package Day_0_Hello_World;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        String inputString = sc.nextLine();
+        sc.close();
+        
+        System.out.println("Hello, World.");
+        System.out.println(inputString);     
+    }
+}
