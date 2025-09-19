aae5481bc16eb0182f55fde88f4b3d2037bee364
Harish Patil <harishap000@gmail.com>
2025-09-19 10:06:14 +0530

    Updated Patterns

--- 30_Days_of_Code/Patterns/Patterns.java
+++ 30_Days_of_Code/Patterns/Patterns.java
@@ -1,7 +1,7 @@
 class Patterns
 {
     public static void main(String[] args) {
-       pattern12(5); 
+       pattern45('E'); 
     }  
     
     public static void pattern1(int n)
@@ -164,6 +164,107 @@ class Patterns
 
             for (int j = 1; j <= totalCol; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern40(int n)
+    {
+        for (int i = 1; i <= 2 * n; i++) {
+
+            for (int j = 1; j <= 2 * n; j++) {
+                if( j >= 5 - i && j <= i + 3 && j >= i - 3 && j <= 11 - i)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern41(int n)
+    {
+        for (int i = 1; i < 2 * n; i++) {
+
+            for (int j = 0; j <= 2 * n; j++) {
+                if( j >= 5 - i && j <= i + 3 && j >= i - 3 && j <= 11 - i)
+                {
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern42(int n)
+    {
+        for (int i = 0; i <= 2 * n; i++) {
+
+            for (int j = 0; j <= 2 * n; j++) {
+                if(j >= 6 - i && j <= 4 + i && j >= i - 4 && j <= 14 - i)
+                {
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern43(int n)
+    {
+        for (int i = 1; i < 2 * n; i++) {
+
+            for (int j = 1; j < 2 * n; j++) {
+                if(j == 6 - i || j == 4 + i || j == i - 4 || j == 14 - i)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern44(int n)
+    {
+        int k = 1;
+        for (int i = 1; i <= n; i++) { 
+            for (int j = 1; j <= i; j++) {
+                System.out.print(k++ + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern45(char n)
+    {
+        int rows = n - 'A' ;
+        for (int i = 1; i <= rows; i++) {
+            
+            for (int s = 1; s <= rows - i; s++) {
+                System.out.print(" ");
+            }
+            
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch++);
+            }
+            
+            ch -= 2;
+            for (int j = 1; j < i; j++) {
+                System.out.print(ch--);
             }
             System.out.println();
         }
